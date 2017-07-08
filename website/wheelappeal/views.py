4e94e9fe55394e317d31197cf57cf76f0ad30510
EC2 Default User <ec2-user@ip-172-31-34-76.us-east-2.compute.internal>
2017-07-08 16:38:56 +0000

    moved api to django -- STILL NEEDS REST IMPLEMENTATION

--- website/wheelappeal/views.py
+++ website/wheelappeal/views.py
@@ -9,3 +9,6 @@ def index(request):
     # template = loader.get_template('static/index.html')
     context = {}
     return render(request, 'wheelappeal/index.html',context)
+
+def api(request):
+    return HttpResponse('This is our API. It is currently under construction.')
