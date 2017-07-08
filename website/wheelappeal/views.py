d92758625fbebc10bb079b2b80af696689c2b350
Tom Large <tomlarge@Toms-MacBook-Pro.local>
2017-07-08 08:43:38 -0400

    website hosted

---
+++ website/wheelappeal/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    # template = loader.get_template('static/index.html')
+    context = {}
+    return render(request, 'wheelappeal/index.html',context)
